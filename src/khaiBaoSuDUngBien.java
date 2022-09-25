b4209fa6772efe51f1261ff921f7f8f3aad01034
unknown <nguyenmai0102.meo@gmail.com>
2022-09-25 20:53:23 +0700

    noi dung

---
+++ src/khaiBaoSuDUngBien.java
@@ -0,0 +1,16 @@
+public class khaiBaoSuDUngBien {
+    public static void main(String[] args) {
+        int i = 10; // khai báo biến i là kiểu int có giá trị là 10;
+        float f = 20.5f; // khai báo biến f có kiểu là float có giá trị là 20.5
+        boolean b = true; // khai báo biến b có kiểu là boolean có giá trị là true
+        char c = 'a'; // khai báo biến c có kiểu là char có giá trị là a
+        String s = "Hà Nội"; // khai báo biến s có kiểu là string và có giá trị là Hà Nội
+        System.out.println("i ="+i);
+        System.out.println("f ="+ f);
+        System.out.println("b ="+b);
+        System.out.println("c ="+c);
+        System.out.println("s ="+s);
+
+    }
+}
+
